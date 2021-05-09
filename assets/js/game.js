884b8b05f8de00eb4a1c4f7b7ebf478516d8cdf9
Jsieler <joshuasieler@Joshs-Air.home>
2021-05-09 13:56:13 -0600

    bug/fight-skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,44 @@
 //    * Fight all enemy robots
 //    * Defeat each enemy robot
 // "LOSE" - Player robot's health is zero or less
-
-var fight = function(enemy) {  
+    var fightOrSkip = function() {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  
+   // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+    }
+    promptFight = promptFight.toLowerCase();
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip") {
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+        // return true if player wants to leave
+        return true;
+        shop();
+      }
+    }
+  }
+    var fight = function(enemy) {  
    
-    while(enemy.health > 0 && playerInfo.health > 0) {
-        // Alert users that they are starting the round
+    // repeat and execute as long as the enemy-robot is alive 
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        // ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
 
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-        
+        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
         if (promptFight === "skip" || promptFight === "SKIP") {
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
